d10011bda6391f776de1131ea3733c9e67e1e506
davidliberal0 <davidliberal0@gmail.com>
2023-10-09 00:37:58 -0400

    add logic for handling unique identifers and secrets for token request

--- spot.py
+++ spot.py
@@ -5,7 +5,32 @@ import base64
 
 load_dotenv()
 
+# hold credentials - store in seperated .env file - 
+# uniquely identifies app making requests
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
+# Base64 encode the client ID and client secret
+client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+client_credentials_base64 = base64.b64encode(client_credentials.encode())
 
+
+# client_id & client_secret  ---request---> Access Token
+
+
+# Request the access token
+token_url = 'https://accounts.spotify.com/api/token'
+headers = {
+    'Authorization': f'Basic {client_credentials_base64.decode()}'
+}
+data = {
+    'grant_type': 'client_credentials'
+}
+response = requests.post(token_url, data=data, headers=headers)
+
+if response.status_code == 200:
+    access_token = response.json()['access_token']
+    print("Access token obtained successfully.")
+else:
+    print("Error obtaining access token.")
+    exit()
